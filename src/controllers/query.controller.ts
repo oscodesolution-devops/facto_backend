f388a9c379b0af655b567c41c6d6819da2c4ef75
Bifin Babu <bifinbabu088@gmail.com>
2025-03-15 11:59:22 +0530

    style: format

--- src/controllers/query.controller.ts
+++ src/controllers/query.controller.ts
@@ -6,27 +6,27 @@ import { db } from "@/models";
 import { NextFunction, Request, Response } from "express";
 
 export const addQuery = bigPromise(
-    async (req: Request, res: Response, next: NextFunction) => {
-      try {
-        const {name, email, phoneNo, query} = req.body;
-        if(!name || !email || !phoneNo || !query){
-            return next(
-                createCustomError(
-                  "All fields are required", 
-                  StatusCode.BAD_REQ
-                )
-              );
-        }
-        const newQuery = await db.Query.create({
-            name,
-            email,
-            phoneNo,
-            query,
-        })
-        const response = sendSuccessApiResponse("Query added successfully", newQuery);
-        res.status(StatusCode.OK).send(response);
-      } catch (error: any) {
-        next(createCustomError(error.message, StatusCode.INT_SER_ERR));
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { name, email, phoneNo, query } = req.body;
+      if (!name || !email || !phoneNo || !query) {
+        return next(
+          createCustomError("All fields are required", StatusCode.BAD_REQ)
+        );
       }
+      const newQuery = await db.Query.create({
+        name,
+        email,
+        phoneNo,
+        query,
+      });
+      const response = sendSuccessApiResponse(
+        "Query added successfully",
+        newQuery
+      );
+      res.status(StatusCode.OK).send(response);
+    } catch (error: any) {
+      next(createCustomError(error.message, StatusCode.INT_SER_ERR));
     }
-  );
+  }
+);
